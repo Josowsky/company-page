4f3d1c0a9f5f790aacd5d709c8d772be2a9851ea
Josowsky <bartosz@jozwowiak.pl>
2020-03-26 23:27:06 +0100

    Styles for Header

--- components/Header/Header.style.js
+++ components/Header/Header.style.js
@@ -2,15 +2,26 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.header`
   height: 60px;
-  display: flex;
-  align-items: center;
   width: 100%;
+  padding: 40px 48px;
+
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export const StyledLogo = styled.p`
   margin-right: auto;
+  font-size: 46px;
 `;
 
-export const StyledNavigation = styled.div``;
+export const StyledNavigation = styled.div`
+  display: flex;
+`;
 
-export const StyledNavigationLink = styled.a``;
+export const StyledNavigationLink = styled.a`
+  display: block;
+  font-size: 24px;
+  margin-left: 45px;
+`;
